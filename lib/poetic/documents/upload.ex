dd6eb451028d38ef8d119580fc246682acd400aa
jaeyson <contact@jaeyson.com>
2019-10-21 21:33:51 +0700

    start @ SHA-256 function

---
+++ lib/poetic/documents/upload.ex
@@ -0,0 +1,22 @@
+defmodule Poetic.Documents.Upload do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "uploads" do
+    field :content_type, :string
+    field :filename, :string
+    field :hash, :string
+    field :size, :integer
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(upload, attrs) do
+    upload
+    |> cast(attrs, [:filename, :size, :content_type, :hash])
+    |> validate_required([:filename, :size, :content_type, :hash])
+    |> validate_number(:size, greater_than: 0) #doesn't allow empty files
+    |> validate_length(:hash, is: 64)
+  end
+end
